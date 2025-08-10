1f19f55554d64225e7fb4ae24f86a5891e74fd94
AdelineXinyi <xinyiade@umich.edu>
2025-08-10 02:07:07 -0400

    fix: no white shine, still have obvious loading process, consider optimize code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,8 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#8b5cf6" />
       </head>
-      <body className={inter.className}>
+      <body className={`${inter.className} dark-background`}>
+
         <ThreeBackgroundWrapper />
         <div className="main-content-container">
          {children}
